c0e962bd3d05faf25419d9d62f33ea5aaca397ff
Marsudi <49959757+Marsudii@users.noreply.github.com>
2022-09-06 22:14:30 +0700

    Update locationDetail.js

--- locationDetail.js
+++ locationDetail.js
@@ -1,62 +1,62 @@
-var $locationText = $(".location")
+  var $locationText = $(".location")
 
-let $latAbsen = $(".absenLat")
-let $lngAbsen = $(".absenLng")
+        var $latAbsen = $(".absenLat")
+        var $lngAbsen = $(".absenLng")
 
 
 
-// Check for geolocation browser support and execute success method
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        geoLocationSuccess,
-        geoLocationError, {
-            timeout: 10000
+        // Check for geolocation browser support and execute success method
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                geoLocationSuccess,
+                geoLocationError, {
+                    timeout: 10000
+                }
+            )
+        } else {
+            alert("your browser doesn't support geolocation")
         }
-    )
-} else {
-    alert("your browser doesn't support geolocation")
-}
 
-function geoLocationSuccess(pos) {
-    // get user lat,long
-    var myLat = pos.coords.latitude,
-        myLng = pos.coords.longitude,
-        loadingTimeout
+        function geoLocationSuccess(pos) {
+            // get user lat,long
+            var myLat = pos.coords.latitude,
+                myLng = pos.coords.longitude,
+                loadingTimeout
 
-    var loading = function() {
-        $locationText.text("fetching...")
-    }
-
-    loadingTimeout = setTimeout(loading, 600)
-
-    var request = $.get(
-            "https://nominatim.openstreetmap.org/reverse?format=json&lat=" +
-            myLat +
-            "&lon=" +
-            myLng
-        )
-        .done(function(data) {
-            if (loadingTimeout) {
-                clearTimeout(loadingTimeout)
-                loadingTimeout = null
-                $locationText.text(data.display_name)
-                $latAbsen.text(myLat)
-                $lngAbsen.text(myLng)
-                $("#myid").val(data.display_name)
-
-
+            var loading = function() {
+                $locationText.text("fetching...")
             }
-        })
-        .fail(function() {
-            // handle error
-        })
-}
 
-function geoLocationError(error) {
-    var errors = {
-        1: "Permission denied",
-        2: "Position unavailable",
-        3: "Request timeout",
-    }
-    alert("Error: " + errors[error.code])
-}
+            loadingTimeout = setTimeout(loading, 600)
+
+            var request = $.get(
+                    "https://nominatim.openstreetmap.org/reverse?format=json&lat=" +
+                    myLat +
+                    "&lon=" +
+                    myLng
+                )
+                .done(function(data) {
+                    if (loadingTimeout) {
+                        clearTimeout(loadingTimeout)
+                        loadingTimeout = null
+                        $locationText.text(data.display_name)
+                        $latAbsen.text(myLat)
+                        $lngAbsen.text(myLng)
+                        $("#myid").val(data.display_name)
+
+
+                    }
+                })
+                .fail(function() {
+                    // handle error
+                })
+        }
+
+        function geoLocationError(error) {
+            var errors = {
+                1: "Permission denied",
+                2: "Position unavailable",
+                3: "Request timeout",
+            }
+            alert("Error: " + errors[error.code])
+        }
